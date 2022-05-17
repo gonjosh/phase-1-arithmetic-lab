ccb307f1bc66dd242eac779dde71815a8e447415
Joshua Gonzalez <gonjosh@gmail.com>
2022-05-17 10:35:57 -0400

    commit tree 69462b7e2011f7318700d1495245b7e36ebd1e14

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random()*10)
+
+let num3 = 12
+let num4 = 3
+const mod = num3 / num4
+
+
+const max = Math.max(5, 10, 15, 20)
+
 
 // Write your code here
